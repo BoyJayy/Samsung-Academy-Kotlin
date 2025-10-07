aae02c061e99a83453b586798534ed2f9b73e271
BoyJayy <popkov.ad@icloud.com>
2025-10-07 16:25:04 +0300

    Create 1-4-1.kt
    1 4 1

---
+++ Практики/1-4-1.kt
@@ -0,0 +1,32 @@
+enum class Direction {
+    UP, DOWN, RIGHT, LEFT
+}
+class Robot(var x: Int, var y: Int, var direction: Direction) {
+    fun turnLeft() {
+        direction = when (direction) {
+            Direction.UP -> Direction.LEFT
+            Direction.LEFT -> Direction.DOWN
+            Direction.DOWN -> Direction.RIGHT
+            Direction.RIGHT -> Direction.UP
+        }
+    }
+    fun turnRight() {
+        direction = when (direction) {
+            Direction.UP -> Direction.RIGHT
+            Direction.RIGHT -> Direction.DOWN
+            Direction.DOWN -> Direction.LEFT
+            Direction.LEFT -> Direction.UP
+        }
+    }
+    fun stepForward() {
+        when (direction) {
+            Direction.UP -> y++
+            Direction.DOWN -> y--
+            Direction.RIGHT -> x++
+            Direction.LEFT -> x--
+        }
+    }
+    override fun toString(): String {
+        return "x: $x, y: $y, dir: $direction"
+    }
+}
